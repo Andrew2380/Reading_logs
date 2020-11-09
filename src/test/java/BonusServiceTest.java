7127aca772807533b598ccce5045b968153255b8
Andrew2380 <andrey238054@gmail.com>
2020-11-09 23:49:23 +0300

    Commit1

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void shouldCalculateRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+
+    }
+}
